05f8e6ab3f43ff38c1e8e70c880aa6aa46530c9b
rusweek <rusweek@gmail.com>
2018-07-08 15:32:47 +0400

    Подготовка к слиянию

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 window.addEventListener('load', function(e){
     let mainContainer = document.querySelector('.mainContainer');
     
+    
     let inviteList = document.querySelector('.inviteList');
     let item = document.querySelectorAll('.item');
     let btnAdd = document.querySelector('.btnAdd');
     
     let btnClear = document.querySelector('.btnClear');
     let wrapper = document.querySelector('.wrapper');
+    let wrapper2 = document.querySelector('.wrapper2');
+
+
+    // условие для загрузки из локального хранилища
+    if(this.localStorage.length>0){
+        LocalStForMyApp();
+    };
     
     //модуль для перемещения item из  mainContainer в inviteList
     mainContainer.addEventListener('click', function (e) { 
@@ -50,5 +58,32 @@ window.addEventListener('load', function(e){
         }
     });
     //конец модуля для перемещения item из    inviteList в mainContainer
+    
+    // мидуль для внесения изменений в локальное хранилице
+
+    mainContainer.addEventListener('click', function () {
+        localStorage.setItem(1, wrapper.innerHTML);
+        
+        LocalStForMyApp();
+    });
+
+    inviteList.addEventListener('click', function () {
+        localStorage.setItem(1, wrapper.innerHTML);
+        
+        LocalStForMyApp();
+    });
+
+    function LocalStForMyApp(){
+        if (localStorage.length > 0) {
+            for (let i = 0; i < localStorage.length; i++) {
+                let key = localStorage.key(i);
+                let item = localStorage.getItem(key);
+                let item2 = localStorage[key];
+                wrapper2.innerHTML = item2;
+                
+                
+            };
+        };
+    };
 
 });
